54af18eeb13333f79ef0f4fdc9fa6d35723f14e0
Pepe G. Krumminga <pepe.krumminga@study.hs-duesseldorf.de>
2024-05-07 20:32:36 +0200

    Added back the network and started work on balancedaccuracy.py

--- balancedaccuracy.py
+++ balancedaccuracy.py
@@ -4,7 +4,7 @@ import torch
 # The balanced accuracy is defined as the average accuracy for each class. 
 # The accuracy for an indiviual class is the ratio between correctly classified example to all examples of that class.
 # The code in train.py will instantiate one instance of this class.
-# It will call the reset methos at the beginning of each epoch. Use this to reset your
+# It will call the reset method at the beginning of each epoch. Use this to reset your
 # internal states. The update method will be called multiple times during an epoch, once for each batch of the training.
 # You will receive the network predictions, a Tensor of Size (BATCHSIZExCLASSES) containing the logits (output without Softmax).
 # You will also receive the groundtruth, an integer (long) Tensor with the respective class index per example.
@@ -17,13 +17,16 @@ class BalancedAccuracy:
     def __init__(self, nClasses):
         # TODO: Setup internal variables
         # NOTE: It is good practive to all reset() from here to make sure everything is properly initialized
-
+        self.nClasses = nClasses
+        self.reset()
     
         def reset(self):
-
         # TODO: Reset internal states.
         # Called at the beginning of each epoch
-            pass
+            self.TP = torch.zeros(self.nClasses)
+            self.TN = torch.zeros(self.nClasses)
+            self.FP = torch.zeros(self.nClasses)
+            self.FN = torch.zeros(self.nClasses)
 
     def update(self, predictions, groundtruth):
         # TODO: Implement the update of internal states
@@ -35,9 +38,16 @@ class BalancedAccuracy:
         #
         # Groundtruth is a BATCH_SIZE x 1 long Tensor. It contains the index of the
         # ground truth class.
-        pass
+        _, predicted_classes = torch.max(predictions, 1)
+        for i in range(self.nClasses):
+            self.TP[i] += predicted_classes[i]
 
     def getBACC(self):
         # TODO: Calculcate and return balanced accuracy 
         # based on current internal state
-        pass
+        
+        sensitivity = TP/(TP+FN)
+        specificity = TN/(TN+FP)
+        bacc = (sensitivity+specificity)/2
+        
+        return bacc
--- network.py
+++ network.py
@@ -12,10 +12,22 @@ class Net(nn.Module):
         # Input imags will be 3 channels 256x256 pixels.
         # Output must be a nClasses Tensor.
         
+        relu = nn.ReLU()
+        pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.net = nn.Sequential(
+        nn.Conv2d(3, 16, kernel_size=(5,5), padding="same"), relu,
+        nn.Conv2d(16, 32, kernel_size=(5,5), padding="same"), relu, pool,
+        nn.Conv2d(32, 64, kernel_size=(5,5), padding="same"), relu,
+        nn.Conv2d(64, 128, kernel_size=(5,5), padding="same"), relu, pool,
+        nn.Conv2d(128, 256, kernel_size=(5,5), padding="same"), relu,
+        nn.Conv2d(256, 512, kernel_size=(5,5), padding="same"), relu, pool,
+        nn.Flatten(), nn.Linear(262144, nClasses))
+        
+        
 
     def forward(self, x):
         # TODO: 
         # Implement forward pass
         # x is a BATCH_SIZEx3x256x256 Tensor
         # return value must be a BATCH_SIZExN_CLASSES Tensor
-        pass
+        return self.net(x)
